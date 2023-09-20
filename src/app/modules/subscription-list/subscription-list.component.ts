00e88722b22784f82e1866a68e2e257fcf222356
gayusuryu <31695246+gayusuryu@users.noreply.github.com>
2023-09-20 21:34:16 +0530

    1 file modified

--- src/app/modules/subscription-list/subscription-list.component.ts
+++ src/app/modules/subscription-list/subscription-list.component.ts
@@ -4,9 +4,15 @@ import {
   Input,
   OnChanges,
   OnInit,
+  SimpleChanges,
 } from '@angular/core';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {
+  map,
+  debounceTime,
+  distinctUntilChanged,
+  switchMap,
+} from 'rxjs/operators';
 import { SubscriptionDataService } from '../../data-services/subscriptions.data-service';
 import { Subscription } from '../../models/subscription.model';
 
@@ -20,18 +26,30 @@ export class SubscriptionListComponent implements OnChanges {
   _subscriptions$: Observable<Subscription[]> =
     this.subscriptionDataService.getSubscriptions();
 
-  @Input() selectedId?: number;
+  @Input() selectedId?: any;
 
-  constructor(private subscriptionDataService: SubscriptionDataService) {
-  }
+  constructor(private subscriptionDataService: SubscriptionDataService) {}
 
-  ngOnChanges() {
+  ngOnChanges(changes: SimpleChanges) {
     console.log(this.selectedId);
+    console.log('change', changes.selectedId.currentValue);
+
+    // this._subscriptions$ = this.selectedId.
     this._subscriptions$ = this._subscriptions$.pipe(
       map((user) => {
         console.log('user1', user);
         return user;
       })
+    );
+
+    this._subscriptions$ = changes.selectedId.currentValue.tap(
+      debounceTime(100),
+      distinctUntilChanged(),
+      this._subscriptions$.pipe(
+        switchMap((subscription) =>
+          subscription.filter((val) => val.id === this.selectedId)
+        )
+      )
     );
   }
 }
