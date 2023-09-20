647e85cab391987275fe385227ffe83922a2659e
gayusuryu <31695246+gayusuryu@users.noreply.github.com>
2023-09-20 21:15:28 +0530

    1 file modified

--- src/app/modules/subscription-list/subscription-list.component.ts
+++ src/app/modules/subscription-list/subscription-list.component.ts
@@ -2,6 +2,7 @@ import {
   ChangeDetectionStrategy,
   Component,
   Input,
+  OnChanges,
   OnInit,
 } from '@angular/core';
 import { Observable } from 'rxjs';
@@ -15,17 +16,20 @@ import { Subscription } from '../../models/subscription.model';
   styleUrls: ['./subscription-list.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SubscriptionListComponent {
+export class SubscriptionListComponent implements OnChanges {
   _subscriptions$: Observable<Subscription[]> =
     this.subscriptionDataService.getSubscriptions();
 
   @Input() selectedId?: number;
 
   constructor(private subscriptionDataService: SubscriptionDataService) {
-   
+  }
+
+  ngOnChanges() {
+    console.log(this.selectedId);
     this._subscriptions$ = this._subscriptions$.pipe(
       map((user) => {
-        console.log('user', user);
+        console.log('user1', user);
         return user;
       })
     );
