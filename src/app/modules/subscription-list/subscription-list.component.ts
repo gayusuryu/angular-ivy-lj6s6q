e66db5d31169b1840633ea58797a7356b7946d6a
gayusuryu <31695246+gayusuryu@users.noreply.github.com>
2023-09-22 16:06:57 +0530

    2 files modified

--- src/app/modules/subscription-list/subscription-list.component.ts
+++ src/app/modules/subscription-list/subscription-list.component.ts
@@ -26,17 +26,18 @@ import { Subscription } from '../../models/subscription.model';
 export class SubscriptionListComponent implements OnChanges {
   _subscriptions$: Observable<Subscription[]> =
     this.subscriptionDataService.getSubscriptions();
+  subscription: any = [];
 
   @Input() selectedId?: any;
 
   constructor(
-    private subscriptionDataService: SubscriptionDataService
-  ) // private cdr: ChangeDetectorRef
-  {}
+    private subscriptionDataService: SubscriptionDataService // private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnChanges(changes: SimpleChanges) {
     console.log(changes);
     console.log('change', changes.selectedId.currentValue);
+    this.subscription = [];
 
     // this._subscriptions$ = changes.selectedId.currentValue.tap(
     //   debounceTime(100),
@@ -48,16 +49,30 @@ export class SubscriptionListComponent implements OnChanges {
     //   )
     // );
 
-    this._subscriptions$ = this._subscriptions$.pipe(
-      map((subs) =>
-        subs.filter((val) => val.userId === changes.selectedId.currentValue)
-      )
-    );
+    // this._subscriptions$
+    //   .pipe(
+    //     switchMap((subs) =>
+    //       subs.filter((val) => val.userId === changes.selectedId.currentValue)
+    //     )
+    //   )
+    //   .subscribe((data) => {
+    //     console.log('dataa', data);
+
+    //     this.subscription.push(data);
+    //   });
 
     // this.cdr.detectChanges();
 
-    // subscription.subscribe((data) => {
-    //   console.log('dataa', data);
-    // });
+    this._subscriptions$
+      .pipe(
+        switchMap((subs) =>
+          subs.filter((val) => val.userId === changes.selectedId.currentValue)
+        )
+      )
+      .subscribe((data) => {
+        console.log('dataa', data);
+
+        this.subscription.push(data);
+      });
   }
 }
